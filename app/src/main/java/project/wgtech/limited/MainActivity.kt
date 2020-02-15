5e12dfbe2dfd3d30bb221613360907e9b8c909c6
WangGi Lim <wgtech.dev@gmail.com>
2020-02-15 10:27:29 +0900

    Add dependencies, implementations and MainActivity

---
+++ app/src/main/java/project/wgtech/limited/MainActivity.kt
@@ -0,0 +1,12 @@
+package project.wgtech.limited
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

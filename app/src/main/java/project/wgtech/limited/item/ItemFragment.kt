d074a8acffe607034796c9ab3a422c022dc46cb9
WangGi Lim <wgtech.dev@gmail.com>
2020-03-13 00:42:33 +0900

    [Fix] Stabilize the moving transition between MainFragment/ItemListRecyclerAdapter and ItemFragment   - Stabilize commit id e064b630d9b5931fecd5b3e56d08a590a5750325 (branch dev/sprint-build)

--- app/src/main/java/project/wgtech/limited/item/ItemFragment.kt
+++ app/src/main/java/project/wgtech/limited/item/ItemFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import project.wgtech.limited.R
 import project.wgtech.limited.databinding.FragmentItemBinding
 
